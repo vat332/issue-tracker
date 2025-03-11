32f3171ae04c2d2f58c6edaf34d1ebee72eddf3e
vat332 <oporowymakus21@gmail.com>
2025-03-11 13:09:30 +0100

    Build an api for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -25,3 +25,19 @@ export async function PATCH(
 
   return NextResponse.json(updatedIssue, { status: 200 });
 }
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "invalid issue" }, { status: 404 });
+
+  await prisma.issue.delete({ where: { id: parseInt(params.id) } });
+
+  return NextResponse.json({});
+}
