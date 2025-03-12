d7776b70a68337746c4c2b788ca5ed2f2a9bf774
vat332 <oporowymakus21@gmail.com>
2025-03-12 22:02:32 +0100

    Implement Pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>Hello world</div>;
+import Pagination from "./components/Pagination";
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
+  return (
+    <Pagination
+      itemCount={50}
+      pageSize={5}
+      currentPage={parseInt(searchParams.page)}
+    />
+  );
 }
